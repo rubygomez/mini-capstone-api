55c2c3249ccfc1531e999b2df666132413856034
rubygomez <rvbii.gomez@gmail.com>
2024-02-27 10:59:54 -0800

    model (not controller) + attributes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'product/name:string'
-  get 'product/price:integer'
-  get 'product/image_url:string'
-  get 'product/description:string'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
