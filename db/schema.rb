358467753a9287acc8c7ed77097096eca8b142c4
rubygomez <rvbii.gomez@gmail.com>
2024-04-02 10:44:02 -0700

    category + categoryproduct models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_22_163546) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_25_191951) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_22_163546) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
